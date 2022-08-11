c3f28606a72903ff13a63023d9a1bf56f4ddc686
Your Name <you@example.com>
2022-08-11 04:20:11 -1200

    update

--- contracts/Betting.sol
+++ contracts/Betting.sol
@@ -4,39 +4,98 @@ pragma solidity ^0.8.0;
 contract Betting{
     event PlaceBet(address caller, uint choice);
     event Deposit(address caller, uint amount);
+    event Launch(uint ID, uint32 StartAt, uint32 EndAt);
 
-    struct Users{
+    
+        // string result;
+        uint public TotalBetAmount;
+
+    struct Bet{
+        uint  betAmount;
         address owner;
         uint accountBalance;
-        uint betAmount;
-        uint choice;
         bool hasPlacedBet;
+        Status  choice;
     }
 
-    struct Choice{
-        string homeWin;
-        string homeLoose;
-        string awayWin;
-        string awayLoose;
-        string draw;
+    enum Status {
+        homeWin,
+        homeLoose,
+        awayWin,
+        awayLoose,
+        draw
+    }
+    mapping(address => Bet) public usersBet;
+
+    struct Matchs{
+        uint matchID;
+        uint startAt;
+        uint endAt;
+        Outcome outcome;
+
     }
 
-    mapping(address => Users) user;
-    mapping(address => Choice) usersChoice;
+    enum Outcome{
+        homeWin,
+        homeLoose,
+        awayWin,
+        awayLoose,
+        draw
+    }
+
+    enum Result{
+        Success,
+        failed
+    }
+    mapping(uint => Matchs) public game;
+    Result  result;
+
+    
 
     function deposit() payable external{
-        Users storage user1= user[msg.sender];
-        require(msg.value > 0, "invalid amount");
-        user1.owner = msg.sender;
-        user1.accountBalance += msg.value;
+        require(msg.value >= 1 ether, "invalid amount");
+        Bet storage myBet = usersBet[msg.sender];
+        myBet.owner = msg.sender;
+        myBet.accountBalance = myBet.accountBalance + msg.value;
         emit Deposit(msg.sender, msg.value);
     }
-    function Bet(string memory _choice)external {
-        Users storage user1 = user[msg.sender];
-        Choice storage user1choice = usersChoice[msg.sender];
+
+    function launch(uint _ID, uint32 _startAt, uint32 _endAt) external {
+        Matchs storage match1 = game[_ID];
+        require(_startAt >= block.timestamp, "start at < now");
+        require(_endAt >= _startAt, "end at < start at");
+        require(_endAt <= block.timestamp + 90 minutes, "end at > max duration");
+        match1.startAt = _startAt;
+        match1.endAt = _endAt;
+        match1.matchID = _ID;
+        emit Launch(_ID, _startAt, _endAt);
 
     }
-    function getResult() external{}
+    function bet(uint _ID, Status _choice, uint amount)  external {
+        Bet storage myBet = usersBet[msg.sender];
+       Matchs storage match1 = game[_ID];
+        require(block.timestamp <= match1.endAt, "ended");
+        myBet.choice = _choice;
+        myBet.betAmount += amount;
+        TotalBetAmount += amount;
+        myBet.accountBalance = myBet.accountBalance - amount;
+        myBet.hasPlacedBet = true;
+        emit PlaceBet(msg.sender, amount); 
+    }
+
+    // function getResult(uint _ID) external returns(uint Result){
+    //   Matchs storage match1 = game[_ID];
+    //   Bet storage myBet = usersBet[msg.sender];
+    //  require(match1.endAt >= block.timestamp + 90 minutes, "Has not ended");
+    //   require(myBet.hasPlacedBet == true, "Bet first");
+      
+    //   if (myBet.choice() == match1.outcome()) {
+    //         return 0;
+    //     }else {
+    //         return 1;
+    //     }
+   
+    // }
     function claim() external{}
     function reward() external{}
 }
